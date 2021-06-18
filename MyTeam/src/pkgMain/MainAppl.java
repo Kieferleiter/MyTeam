1c8857f8072cdedc6a33de748a53a6cfd653fe7e
David R <46679013+Kieferleiter@users.noreply.github.com>
2021-06-18 11:21:03 +0200

    Update MainAppl.java

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -7,6 +7,9 @@ public class MainAppl {
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println(player);
+		Player player2 = new Player(4, "Hinteregger", "Defense");
+		System.out.println(player2);
+		
 
 	}
 
