556e7de05d19f14790c40fd97dd18f4798917ec8
schueler <schueler@ubuntu>
2021-06-21 16:20:38 +0200

    +Alaba -Dragovic

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -5,7 +5,7 @@ import pkgData.Player;
 public class MainAppl {
 
 	public static void main(String[] args) {
-		Player player = new Player(2, "Dragovic", "Defense");
+		Player player = new Player(2, "Alaba", "Defense");
 		System.out.println(player);
 		
 		player = new Player(8, "Alaba", "Defense");
