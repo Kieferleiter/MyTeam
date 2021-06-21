86761ce06aa7eb53caca36b1c21c1e1bcb8f1cce
David R <46679013+Kieferleiter@users.noreply.github.com>
2021-06-21 15:23:12 +0200

    + Alaba

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -7,9 +7,11 @@ public class MainAppl {
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println(player);
-		Player player2 = new Player(4, "Hinteregger", "Defense");
-		System.out.println(player2);
+		player = new Player(4, "Hinteregger", "Defense");
+		System.out.println(player);
 		
+		player = new Player(8, "Alaba", "Defense");
+		System.out.println(player);
 
 	}
 
