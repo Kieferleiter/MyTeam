6c07eec730d5dd7254a5ba9a1bb45f265c3358a3
schueler <schueler@ubuntu>
2021-06-21 15:39:15 +0200

    removed old players

--- MyTeam/src/pkgMain/MainAppl.java
+++ MyTeam/src/pkgMain/MainAppl.java
@@ -7,8 +7,6 @@ public class MainAppl {
 	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println(player);
-		player = new Player(4, "Hinteregger", "Defense");
-		System.out.println(player);
 		
 		player = new Player(8, "Alaba", "Defense");
 		System.out.println(player);
